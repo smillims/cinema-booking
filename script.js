928c47d3f865b3a89d2eb4f2367b54103c9b04b1
smillims <mrdenkil777@gmail.com>
2021-06-04 20:27:41 +0300

    refactor some of code

--- script.js
+++ script.js
@@ -1,34 +1,32 @@
-const formSeating = document.querySelector('.form-seating');
+const formSeating = document.querySelector(".form-seating");
 
-const asideMenuGreenTicket = document.querySelector('.aside-menu-tickets-green');
-const asideMenuYellowTicket = document.querySelector('.aside-menu-tickets-yellow');
-const asideMenuBlueTicket = document.querySelector('.aside-menu-tickets-blue');
+const asideMenuGreenTicket = document.querySelector(".aside-menu-tickets-green");
+const asideMenuYellowTicket = document.querySelector(".aside-menu-tickets-yellow");
+const asideMenuBlueTicket = document.querySelector(".aside-menu-tickets-blue");
 
 function checkClick(e) {
-	const targetSeating = e.target.closest('.seating-input');
+  const targetSeating = e.target.closest(".seating-input");
 
-	if(!targetSeating) return;
+  if (!targetSeating) return;
 
-	numberOfTickets(targetSeating);
+  numberOfTickets(targetSeating);
 }
 
 function numberOfTickets(target) {
-	const targetInputValue = target.value;
-	const [row, seat] = targetInputValue.split('-');
+  const targetInputValue = target.value;
+  const [row, seat] = targetInputValue.split("-");
 
-	if((target.getAttribute('name') === 'green' || target.getAttribute('name') === 'yellow' || target.getAttribute('name') === 'blue') && target.checked) {
-		generateTicketInfoBlock(target, row, seat, targetInputValue);
-	} else if (!target.checked) {
-		removeTicketInfoBlock(targetInputValue);
-	}
+  if (target && target.checked) generateTicketInfoBlock(target, row, seat, targetInputValue);
+
+  if (!target.checked) removeTicketInfoBlock(targetInputValue);
 }
 
 function generateTicketInfoBlock(target, row, seat, id) {
-	const divMoviePlace = document.createElement('div');
-	
-	divMoviePlace.classList.add('movie-block-inside');
-	divMoviePlace.dataset.id = id;
-	divMoviePlace.innerHTML = `<div class="movie-first-column">
+  const divMoviePlace = document.createElement("div");
+
+  divMoviePlace.classList.add("movie-block-inside");
+  divMoviePlace.dataset.id = id;
+  divMoviePlace.innerHTML = `<div class="movie-first-column">
 											<p class="movie-place">Row: ${row}</p>
 											<p class="movie-row">Place: ${seat}</p>
 										</div>
@@ -38,22 +36,22 @@ function generateTicketInfoBlock(target, row, seat, id) {
 											<p class="movie-text-3">day!</p>
 										</div>`;
 
-	if(target.getAttribute('name') === 'green' && target.checked){
-		asideMenuGreenTicket.appendChild(divMoviePlace);
-	} else if(target.getAttribute('name') === 'yellow' && target.checked) {
-		asideMenuYellowTicket.appendChild(divMoviePlace);
-	} else if(target.getAttribute('name') === 'blue' && target.checked) {
-		asideMenuBlueTicket.appendChild(divMoviePlace);
-	}
+  if (target.getAttribute("name") === "green") {
+    asideMenuGreenTicket.appendChild(divMoviePlace);
+  } else if (target.getAttribute("name") === "yellow") {
+    asideMenuYellowTicket.appendChild(divMoviePlace);
+  } else if (target.getAttribute("name") === "blue") {
+    asideMenuBlueTicket.appendChild(divMoviePlace);
+  }
 }
 
 function removeTicketInfoBlock(id) {
-	const movieBlockInside = document.querySelectorAll('.movie-block-inside');
-	movieBlockInside.forEach(item => {
-		if (item.dataset.id === id){
-			item.remove();
-		}
-	});
+  const movieBlockInside = document.querySelectorAll(".movie-block-inside");
+  movieBlockInside.forEach((item) => {
+    if (item.dataset.id === id) {
+      item.remove();
+    }
+  });
 }
 
-formSeating.addEventListener('click', checkClick);
+formSeating.addEventListener("click", checkClick);
